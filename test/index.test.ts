2518dd800745c2e5cc737bd9e7a73f3d008e8d98
Ofer Shaal <oshaal@phase2technology.com>
2022-07-22 03:02:38 +0000

    add yarn publish

--- test/index.test.ts
+++ test/index.test.ts
@@ -9,44 +9,44 @@ const examples = {
 };
 
 describe.each(Object.entries(examples))('parse()', (label, sample) => {
- test(`${label} does not fail`, () => {
-   parse(sample);
- })
-})
+  test(`${label} does not fail`, () => {
+    parse(sample);
+  });
+});
 
 describe('denormalize()', () => {
   it('should return a single menu array when parsing the main menu', () => {
-   const menus = denormalize(JSON.parse(examples['Main menu']));
-   expect(Array.isArray(menus)).toBe(true);
-   expect((menus as Array<MenuInterface>).length).toBe(1);
+    const menus = denormalize(JSON.parse(examples['Main menu']));
+    expect(Array.isArray(menus)).toBe(true);
+    expect((menus as Array<MenuInterface>).length).toBe(1);
   });
   it('should return a menu with a correctly structured tree', () => {
-   const menu = denormalize(JSON.parse(examples['Main menu']), 'main') as MenuInterface;
-   expect(menu.tree[0].title).toBe('Home');
-   expect(menu.tree[1].title).toBe('About us');
-   expect(menu.tree[1].children[0].title).toBe('Our name');
+    const menu = denormalize(JSON.parse(examples['Main menu']), 'main') as MenuInterface;
+    expect(menu.tree[0].title).toBe('Home');
+    expect(menu.tree[1].title).toBe('About us');
+    expect(menu.tree[1].children[0].title).toBe('Our name');
   });
   it('should return a single menu when parsing the main menu and passing a menu ID', () => {
-   const menus = denormalize(JSON.parse(examples['Main menu']), 'main');
-   expect(Array.isArray(menus)).toBe(false);
+    const menus = denormalize(JSON.parse(examples['Main menu']), 'main');
+    expect(Array.isArray(menus)).toBe(false);
   });
   it('should return two menus when parsing the combined menu', () => {
-   const menus = denormalize(JSON.parse(examples['Combined menu']));
-   expect(Array.isArray(menus)).toBe(true);
-   expect((menus as Array<MenuInterface>).length).toBe(2);
+    const menus = denormalize(JSON.parse(examples['Combined menu']));
+    expect(Array.isArray(menus)).toBe(true);
+    expect((menus as Array<MenuInterface>).length).toBe(2);
   });
   it('should return a single menu when parsing the combined menu and passing a menu ID', () => {
-   const menus = denormalize(JSON.parse(examples['Combined menu']), 'account');
-   expect(Array.isArray(menus)).toBe(false);
+    const menus = denormalize(JSON.parse(examples['Combined menu']), 'account');
+    expect(Array.isArray(menus)).toBe(false);
   });
   it('should return a single menu when parsing the contextual menu and passing a menu ID', () => {
-   const menus = denormalize(JSON.parse(examples['Combined menu']), 'account');
-   expect(Array.isArray(menus)).toBe(false);
+    const menus = denormalize(JSON.parse(examples['Combined menu']), 'account');
+    expect(Array.isArray(menus)).toBe(false);
   });
   it('should return an account menu and a main menu when parsing the contextual menu', () => {
-   const menus = denormalize(JSON.parse(examples['Contextual menu'])) as Array<MenuInterface>;
-   expect(menus[0].tree[0].link.attributes['drupal-menu-machine-name'][0]).toBe('account');
-   expect(menus[1].tree[0].link.attributes['drupal-menu-machine-name'][0]).toBe('main');
+    const menus = denormalize(JSON.parse(examples['Contextual menu'])) as Array<MenuInterface>;
+    expect(menus[0].tree[0].link.attributes['drupal-menu-machine-name'][0]).toBe('account');
+    expect(menus[1].tree[0].link.attributes['drupal-menu-machine-name'][0]).toBe('main');
   });
   it('should return a correctly ordered menu from the contextual account menu', () => {
     const menu = denormalize(JSON.parse(examples['Contextual menu']), 'account') as MenuInterface;
